5caf442ef922fa4b7c4cf4145b28777b502560fa
fauzandp <fauzan27dp@gmail.com>
2023-05-07 19:28:18 +0700

    Project15 | Animation add button using storyboard add imageView programmatically add logic to handle which animtion is add animation to the view with UIView.animate()

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = view.center
+        imageView.layer.cornerRadius = 30
+        view.addSubview(imageView)
+        
     }
 
-
+    @IBAction func Tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -200, y: -200)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.backgroundColor?.withAlphaComponent(0.1)
+                self.imageView.backgroundColor = .yellow
+            case 7:
+                self.imageView.backgroundColor?.withAlphaComponent(1)
+                self.imageView.backgroundColor = .clear
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
